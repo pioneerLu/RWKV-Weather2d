70b0ae41f0140a93158748ad531422dbb86478b5
Gary <1601978618@qq.com>
2024-12-05 00:05:40 +0800

    'update'

--- dataset_3d.py
+++ dataset_3d.py
@@ -8,16 +8,24 @@ from tqdm import tqdm
 import random
 
 class RWKVWeatherDataset(Dataset):
-    def __init__(self, data_path,seq_len=10,max_len=50,output_len=10):
+    def __init__(self, data_path,seq_len=10,max_len=50,output_len=10,flag='train',split=0.8):
+
         assert seq_len <= max_len//2 
+        assert flag in ['train', 'val']
+        assert split >= 0 and split <=1.0
         self.path = data_path
         self.seq_len = seq_len
         self.max_len = max_len
         self.output_len = output_len
+        self.split = split
+        type_map = {'train': 0, 'val': 1}
+        self.set_type = type_map[flag]
         self.chunks = []
         self.__read_data__()
 
     def __read_data__(self):
+
+        
         data_raw = xr.open_mfdataset(self.path, combine='by_coords')
         temp = []
         for data_name in data_raw.data_vars:
@@ -35,16 +43,19 @@ class RWKVWeatherDataset(Dataset):
             chunk = temp[:, start_idx:end_idx, :, :]
             self.chunks.append(chunk) # (C, T, H, W)
 
-
+        num_train = int(len(self.chunks) * self.split)###
+        print(f"Number of {self.set_type} samples: {num_train}")
+        border1s = [0, num_train]
+        border2s = [num_train, len(self.chunks)]
+        border1,border2 = border1s[self.set_type],border2s[self.set_type]
+        self.chunks = self.chunks[border1:border2]
     
     def __getitem__(self, idx):
 
-        """
-            这里暂时返回整个对应索引的数据，后续需要完善。
-        """
         s = random.randrange(0,self.max_len-3*self.seq_len) ###
         input_points = []
         target = []
+
         for chunk in self.chunks:
             s = random.randrange(0,self.max_len-2*self.seq_len) 
             input_data = chunk[:, s:s+self.seq_len, :, :]
@@ -56,7 +67,10 @@ class RWKVWeatherDataset(Dataset):
             'input': np.concatenate(input_points, axis=1),
             'target': np.concatenate(target, axis=1),
         }
-
+        # sample = {
+        #     'input': np.array(input_points),
+        #     'target': np.array(target),
+        # }
         return sample
 
     def __len__(self):
