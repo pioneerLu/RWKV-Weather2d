d56d1adb246ff8d8b5737586508358f0a0286089
Gary <1601978618@qq.com>
2024-12-04 13:59:42 +0800

    'update'

--- dataset_3d.py
+++ dataset_3d.py
@@ -8,9 +8,12 @@ from tqdm import tqdm
 import random
 
 class RWKVWeatherDataset(Dataset):
-    def __init__(self, data_path,seq_len=10,input_len=10,output_len=1):
+    def __init__(self, data_path,seq_len=10,max_len=50,output_len=10):
+        assert seq_len <= max_len//2 
         self.path = data_path
         self.seq_len = seq_len
+        self.max_len = max_len
+        self.output_len = output_len
         self.chunks = []
         self.__read_data__()
 
@@ -24,12 +27,12 @@ class RWKVWeatherDataset(Dataset):
 
         time_dim_length = temp.shape[1]
 
-        for start_idx in range(0, time_dim_length, self.seq_len):
-            end_idx = start_idx + self.seq_len
+        for start_idx in range(0, time_dim_length, self.max_len):
+            end_idx = start_idx + self.max_len
             if end_idx > time_dim_length:
                 end_idx = time_dim_length
             chunk = temp[:, start_idx:end_idx, :, :]
-            self.chunks.append(chunk)
+            self.chunks.append(chunk) # (C, T, H, W)
 
 
     
@@ -38,10 +41,11 @@ class RWKVWeatherDataset(Dataset):
         """
             这里暂时返回整个对应索引的数据，后续需要完善。
         """
-        s = random.randrange(0,self.seq_len)###
 
-        input_data = self.chunks[:, idx:idx + self.input_len, :, :]
-        target_data = self.chunks[:, idx + self.input_len:idx + self.input_len + self.output_len, :, :]
+        s = random.randrange(0,self.max_len-2*self.seq_len) ###
+
+        input_data = self.chunks[:, s:s+self.seq_len, :, :]
+        target_data = self.chunks[:, s+self.seq_len:s+self.seq_len+self.output_len, :, :]
 
         sample = {
             'input': input_data,
